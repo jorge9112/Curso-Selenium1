82668dd7f0a58b199154827aef2df4619d87548f
Prueba <a_herrerajol@globalhitss.com>
2020-12-24 11:20:38 -0600

    creacion prueba

---
+++ src/test/java/login/LoginTests.java
@@ -0,0 +1,21 @@
+package login;
+
+import base.BaseTests;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.LoginPage;
+import pages.SecureAreaPage;
+
+import static org.testng.Assert.assertEquals;
+
+public class LoginTests extends BaseTests {
+
+    @Test
+    public void testSuccessfulLogin(){
+        LoginPage loginPage = homePage.clickFormAuthentication();
+        loginPage.setUsername("tomsmith");
+        loginPage.setPassword("SuperSecretPassword!");
+        SecureAreaPage secureAreaPage = loginPage.clickLoginButton();
+        assertEquals(secureAreaPage.getAlertText(), "You logged into a secure area! x", "Alert text is incorrect");
+    }
+}
