660a6a16a3cb511b06c1372bae16fab74ec5c8aa
Prueba <a_herrerajol@globalhitss.com>
2020-12-28 13:14:33 -0600

    Carga de Archivos

--- src/test/java/alerts/FileUploadTests.java
+++ src/test/java/alerts/FileUploadTests.java
@@ -1,2 +1,14 @@
-package alerts;public class FileUploadTests {
+package alerts;
+
+import base.BaseTests;
+import org.testng.annotations.Test;
+
+public class FileUploadTests extends BaseTests {
+
+    @Test
+    public void testFileUpload(){
+        var uploadPage = homePage.clickFileUpload();
+        uploadPage.uploadFile("C:\Users\Miracle\IdeaProjects\webdriver_java\resources\chromedriver.exe");
+        assertEquals(uploadPage.getUploadFiles(), "chromedriver"
+    }
 }
